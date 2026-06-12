4555cf33d3f6e8c985b1052d554bba642b762b22
Chloecx <72976488+Chloecx@users.noreply.github.com>
2026-06-12 16:51:04 +0900

    Update merge_videos.py

--- merge_videos.py
+++ merge_videos.py
@@ -1,16 +1,43 @@
 import os
 
-command = """
-ffmpeg \
--i clean/sad_woman.mp4 \
--i clean/thinking_person.mp4 \
--i clean/city_night.mp4 \
--filter_complex "[0:v][1:v][2:v]concat=n=3:v=1:a=0[outv]" \
--map "[outv]" \
--c:v libx264 \
-final.mp4
-"""
+videos = sorted(
+    [
+        f
+        for f in os.listdir("clips")
+        if f.endswith(".mp4")
+    ]
+)
+
+if len(videos) == 0:
+    print("clips目录为空")
+    raise SystemExit
+
+inputs = ""
+
+for video in videos:
+    inputs += f'-i "clips/{video}" '
+
+concat_inputs = ""
+
+for i in range(len(videos)):
+    concat_inputs += f'[{i}:v]'
+
+filter_complex = (
+    f'"{concat_inputs}'
+    f'concat=n={len(videos)}:v=1:a=0[outv]"'
+)
+
+command = (
+    f'ffmpeg -y '
+    f'{inputs} '
+    f'-filter_complex {filter_complex} '
+    f'-map "[outv]" '
+    f'-c:v libx264 '
+    f'final.mp4'
+)
+
+print("开始拼接视频...")
 
 os.system(command)
 
-print("视频拼接完成")
+print("视频拼接完成：final.mp4")
